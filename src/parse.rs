f449605115603d66a170ee6cd8988bfddbe29374
roman <rofrol@gmail.com>
2016-09-13 01:32:27 +0200

    json

--- src/parse.rs
+++ src/parse.rs
@@ -4,12 +4,40 @@ use select::predicate::{Predicate, Attr, Class, Name};
 
 fn main() {
     let document = Document::from(include_str!("../thepiratebay--top--207.html"));
-    for node in document.find(Class("detName")).find(Name("a")).iter().take(5) {
-        println!("{}", node.text());
-        for n in node.parent().unwrap().parent().unwrap().find(Name("a")).iter() {
+    println!("[");
+    for node in document.find(Attr("id", "searchResult"))
+        .find(Name("tbody"))
+        .find(Name("tr"))
+        .iter()
+        .take(5) {
+
+        let children = node.children();
+        let mut iter = children.iter();
+        let td_name_magnet = iter.next().unwrap().next().unwrap().next().unwrap().next().unwrap();
+
+        let name_selection = td_name_magnet.find(Class("detName")).find(Name("a"));
+        let mut name_selection_iter = name_selection.iter();
+        let name = name_selection_iter.next().unwrap().text();
+
+        let mut magnet = String::new();
+
+
+        for n in td_name_magnet.find(Name("a")).iter() {
             if n.attr("href").unwrap().starts_with("magnet:") {
-                println!("{}", n.attr("href").unwrap());
+                magnet = n.attr("href").unwrap().to_string();
+                break;
             }
         }
+
+        let seeders = td_name_magnet.next().unwrap().next().unwrap();
+        let leechers = seeders.next().unwrap().next().unwrap();
+
+        println!("  {{");
+        println!("    name: \"{}\"", name);
+        println!("    magnet: \"{}\"", magnet);
+        println!("    seeders: {}", seeders.text());
+        println!("    leechers: {}", leechers.text());
+        println!("  }},");
     }
+    println!("]");
 }
